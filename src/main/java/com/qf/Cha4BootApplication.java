57b5b0b9cbac5bbad0819cb381aeff4ca29144cf
ashuai <985486255@qq.com>
2021-08-03 18:45:21 +0800

    项目经理首次提交

---
+++ src/main/java/com/qf/Cha4BootApplication.java
@@ -0,0 +1,13 @@
+package com.qf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cha4BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cha4BootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qf/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.qf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    //上传代码
+    @GetMapping("/info")
+    public String info(){
+        return "Hello qfjy SpringBoot";
+
+    }
+}
